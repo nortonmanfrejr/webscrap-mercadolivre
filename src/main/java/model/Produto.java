b39ea0e26fb42acea8737925423af56fe81c8fdd
nortonmanfre <nortonmanfrejr@hotmail.com>
2022-06-30 23:03:04 -0300

    300622, layout 1 concluido e modelo produto criado

---
+++ src/main/java/model/Produto.java
@@ -0,0 +1,39 @@
+package model;
+
+public class Produto {
+
+    private String nome;
+    private String valor;
+    private String desconto;
+
+    public Produto(){
+        super();
+    }
+
+    //#region Getters and Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public String getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(String desconto) {
+        this.desconto = desconto;
+    }
+
+    //endregion
+}
